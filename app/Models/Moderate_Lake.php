e919e843631b8acec41736e8df26c4914f419755
Njeri <85888329+Njeri-Ngugi@users.noreply.github.com>
2024-01-27 21:27:11 +0300

    Project setup

---
+++ app/Models/Moderate_Lake.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Moderate_Lake extends Model
+{
+    use HasFactory;
+}
